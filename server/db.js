c2af2585f01ecbebf6b2cb0a0a56099da3a425cb
Тахциди Максим Димитриевич <29424196@s.mfui.ru>
2025-10-09 14:35:01 +0300

    Add some information about project

---
+++ server/db.js
@@ -0,0 +1,13 @@
+import {Sequlize} from 'sequlize'
+
+const sequelize = new Sequelize(
+	process.env.DB_NAME,
+	process.env.DB_USER,		// общие данные, ниже идут уникальные
+	process.env.DB_PASSWORD, 
+    {
+  host: process.env.DB_HOST,
+  dialect: 'postgres',
+  port: process.env.DB_PORT
+})
+
+export default sequelize;
